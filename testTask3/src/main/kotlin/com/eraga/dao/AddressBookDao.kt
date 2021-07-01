3e935d90b86ac97f33d088718405aa08649d5474
Deneno <deneno@yandex.ru>
2021-07-01 14:41:08 +0300

    исправление ошибок ide

--- testTask3/src/main/kotlin/com/eraga/dao/AddressBookDao.kt
+++ testTask3/src/main/kotlin/com/eraga/dao/AddressBookDao.kt
@@ -3,6 +3,6 @@ package com.eraga.dao
 import org.springframework.data.repository.CrudRepository
 import com.eraga.model.AddressBook
 
-interface BandDao : CrudRepository<AddressBook, Int> {
+interface AddressBookDao : CrudRepository<AddressBook, Int> {
     fun findByOrderByName(): List<AddressBook>
 }
