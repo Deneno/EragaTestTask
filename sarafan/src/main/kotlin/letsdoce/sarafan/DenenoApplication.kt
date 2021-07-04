6c884705e420480d831be607d06d2d6117b1e00a
Deneno <deneno@yandex.ru>
2021-07-04 21:07:24 +0300

    Изучи 20 раз одно и тоже и возможно запомнишь что-то, но точно узнаешь много нового. Основа: https://www.youtube.com/watch?v=LOmcsf5IylI&list=PLU2ftbIeotGqSTOVNjT4L3Yfy8jatCdhm&ab_channel=letsCode

---
+++ sarafan/src/main/kotlin/letsdoce/sarafan/DenenoApplication.kt
@@ -0,0 +1,11 @@
+package letsdoce.sarafan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DenenoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DenenoApplication>(*args)
+}
---
+++ sarafan/src/test/kotlin/letsdoce/sarafan/DenenoApplicationTests.kt
@@ -0,0 +1,13 @@
+package letsdoce.sarafan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DenenoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
