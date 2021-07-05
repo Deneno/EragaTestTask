c188aced372efef1fedb8df7cf2c1e043c491b0a
Deneno <deneno@yandex.ru>
2021-07-05 15:09:13 +0300

    Изучи 20 раз одно и тоже и возможно запомнишь что-то, но точно узнаешь много нового. Основа: https://www.youtube.com/watch?v=LOmcsf5IylI&list=PLU2ftbIeotGqSTOVNjT4L3Yfy8jatCdhm&ab_channel=letsCode

--- sarafan/src/main/kotlin/letsdoce/sarafan/controller/MessageController.kt
+++ sarafan/src/main/kotlin/letsdoce/sarafan/controller/MessageController.kt
@@ -1,14 +1,28 @@
 package letsdoce.sarafan.controller
 
+import letsdoce.sarafan.exception.NotFoundException
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("message")
 class MessageController {
-    @GetMapping
-    fun list(): String {
-        return "index"
+    fun messages(): List<Map<String, String>> {
+        val message: MutableList<Map<String, String>> = mutableListOf(mutableMapOf("id" to "1", "text" to "First message"))
+        message.add(mutableMapOf("id" to "2", "text" to "Second message"))
+        message.add(mutableMapOf("id" to "3", "text" to "Third message"))
+        return message
     }
+
+    @GetMapping
+    fun list(): List<Map<String, String>> {
+        return messages()
+    }
+    @GetMapping("{id}")
+    fun findById(@PathVariable id: String): Map<String, String>  {
+        return messages().filter{it["id"].equals(id)}.toList().firstOrNull() ?: throw NotFoundException(id)
+
+        }
 }
